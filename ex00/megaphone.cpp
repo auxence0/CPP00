f8060a5a42cc017630142fab5736ef4d91196b23
auxence0 <sauvagetauxence@gmail.com>
2026-06-11 16:46:48 +0200

    voila

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 15:08:04 by asauvage          #+#    #+#             */
-/*   Updated: 2026/05/27 16:15:26 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/06/11 13:15:40 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,5 @@ int main(int ac, char **av)
 				std::cout << char(toupper(av[i][j++]));
 		}
 		std::cout << '\n';
-		
 	}
-	i
 }
